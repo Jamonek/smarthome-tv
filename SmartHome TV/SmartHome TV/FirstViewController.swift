fad0b350694859286bb868623c5b3b3e190240ef
Jamone Kelly <xg.monie089@gmail.com>
2019-01-17 19:18:45 -0500

    Initial commit

---
+++ SmartHome TV/SmartHome TV/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SmartHome TV
+//
+//  Created by Jamone Kelly on 1/17/19.
+//  Copyright © 2019 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
